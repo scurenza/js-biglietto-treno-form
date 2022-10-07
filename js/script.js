f7d69f45167e13fbfd087eb366ec1742f66884b6
scurenza <fedechiar@hotmail.it>
2022-10-07 16:57:25 +0200

    problemi con output

--- js/script.js
+++ js/script.js
@@ -6,12 +6,43 @@ let userPrice = 0;
 
 const submitBtn = document.getElementById("submit");
 
-
+// Prendo gli input
+let userDistanceInput = document.getElementById("userDistance");
+let userAgeInput =document.getElementById("userAge");
 
 // Submit al click del bottone
 submitBtn.addEventListener("click", function() {
-    // Prendo gli input
-    const userDistance = parseInt( document.getElementById("userDistance") );
-    const userAge =parseInt( document.getElementById("userAge") );
+    // elaboro gli input
+    let userDistance = parseInt(userDistanceInput.value);
+    let userAge = parseInt(userAgeInput.value);
     console.log(userDistance, typeof(userDistance));
-})
+    console.log(userAge, typeof(userAge));
+
+    // Elaborazione ed output
+if (userAge < 18 ) {
+    userPrice = basePrice * userDistance ;
+    let userDiscountedPrice = ( userPrice - (userPrice * youngDiscount));
+
+}
+
+else if (userAge >= 65 ) {
+    userPrice = basePrice * userDistance ;
+    let userDiscountedPrice = ( userPrice - (userPrice * oldDiscount));
+    console.log("Prezzo da troncare:",userDiscountedPrice);
+    userDiscountedPrice = Math.trunc(userDiscountedPrice*100)/100;
+    console.log("Prezzo troncato:",userDiscountedPrice);
+}
+
+else {
+    userPrice = basePrice * userDistance ;
+    let userDiscountedPrice = userPrice;
+    console.log("Prezzo da troncare:",userDiscountedPrice);
+    userDiscountedPrice = Math.trunc(userDiscountedPrice*100)/100;
+    console.log("Prezzo troncato:",userDiscountedPrice);
+}
+
+console.log("Prezzo da troncare:",userDiscountedPrice);
+userDiscountedPrice = Math.trunc(userDiscountedPrice*100)/100;
+console.log("Prezzo troncato:",userDiscountedPrice);
+});
+
