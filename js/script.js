d57e59a40b7549f337ec99dceeccb71297cc8b32
scurenza <fedechiar@hotmail.it>
2022-10-07 16:34:38 +0200

    problemi con input

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert("ciao");
+// Dati
+const basePrice = 0.21;
+const youngDiscount = 0.2;
+const oldDiscount = 0.4;
+let userPrice = 0;
+
+const submitBtn = document.getElementById("submit");
+
+
+
+// Submit al click del bottone
+submitBtn.addEventListener("click", function() {
+    // Prendo gli input
+    const userDistance = parseInt( document.getElementById("userDistance") );
+    const userAge =parseInt( document.getElementById("userAge") );
+    console.log(userDistance, typeof(userDistance));
+})
