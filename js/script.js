4844875212afa8675daa302ba1594b9d02a3587b
scurenza <fedechiar@hotmail.it>
2022-10-09 18:31:57 +0200

    migliorie Milestone 2

--- js/script.js
+++ js/script.js
@@ -9,15 +9,19 @@ const cancelBtn = document.getElementById("cancel");
 
 // Prendo gli input
 let userDistanceInput = document.getElementById("userDistance");
-let userAgeInput =document.getElementById("userAge");
+let userAgeInput = document.getElementById("userAge");
+let userNameInput = document.getElementById("userNameInput");
 
 // Submit al click del bottone submitBtn
 submitBtn.addEventListener("click", function() {
     // elaboro gli input
     let userDistance = parseInt(userDistanceInput.value);
     let userAge = parseInt(userAgeInput.value);
+    let userName = userNameInput.value;
+    let unicCode = Math.trunc( (Math.random() * (10000 - 1 + 1) + 1));
     console.log(userDistance, typeof(userDistance));
     console.log(userAge, typeof(userAge));
+    console.log(userName, typeof(userName));
 
     // Elaborazione ed output
     userPrice = basePrice * userDistance ;
@@ -36,9 +40,11 @@ else {
     userPrice = Math.trunc(userPrice*100)/100; 
 }
 
+
 // Ripulisco l'input
 userDistanceInput.value = "";
 userAgeInput.value = "";
+userNameInput.value ="";
 
 
 // Stampiamo a schermo il risultato
@@ -54,6 +60,14 @@ if (document.getElementById('currency').value === 'dollar') {
     document.getElementById("result").innerHTML = (Math.trunc( (userPrice * 0.98)*100)/100) + '$';
     }
 
+if (document.getElementById('userNameInput').value != 0 ) {
+    document.getElementById('UserName').innerHTML = userName;
+}
+
+if (userNameInput.lenght != 0) {
+    document.getElementById('userName').innerHTML = userName;
+    document.getElementById('unicCode').innerHTML = unicCode;
+}
 // Ripulisco campo valuta
 currency.value = "";
 });
